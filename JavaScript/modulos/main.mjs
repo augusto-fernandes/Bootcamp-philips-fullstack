c3d1f357c29173a8435b0b6b8144ef61b6b13a0f
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-12 18:24:50 -0300

    aulas sobre modulos

---
+++ JavaScript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade,mostraIdade, mostraHobby } from "./funcoes.mjs"
+
+console.log(mostraIdade("Augusto", 20));
+console.log(mostraCidade("Augusto", "São Paulo"));
+console.log(mostraHobby("Augusto", "Estudar"));
